dd7e18e433b847a462ffc5a00c432f0f88358d43
Kirill <shkolenko02-03-2004@mail.ru>
2022-11-16 15:12:01 +0300

    First commit

--- Procfile
+++ Procfile
@@ -1,1 +1,6 @@
-web: gunicorn app.wsgi --log--file
+asgiref==3.5.2
+dj-database-url==1.0.0
+Django==4.1.3
+gunicorn==20.1.0
+sqlparse==0.4.3
+whitenoise==6.2.0
--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p+n(v)8bw%k%0@b(-dvl188qpp!axz#v5$t!fj-qmv*^17s_jo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,6 +74,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import dj_database_url
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -81,6 +85,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -117,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
